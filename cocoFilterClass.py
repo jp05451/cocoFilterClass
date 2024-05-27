8bf9568465686b7a173cc5708b7000f19e601499
= <jp05451@yahoo.com.tw>
2024-05-27 17:13:47 +0800

    add tensorflow format

--- cocoFilterClass.py
+++ cocoFilterClass.py
@@ -1,14 +1,16 @@
 import os
-
 from shutil import copyfile
 from pycocotools.coco import COCO
 from xml.dom.minidom import parseString
 from lxml.etree import Element, SubElement, tostring
 from IPython.display import clear_output
 
+IMAGE_FOLDER="train2017"
+LABEL_FOLDER="annotations/instances_train2017.json"
 
+# Function to update and display the progress bar
 def update_progress(progress):
-    bar_length = 20
+    bar_length = 20  # Length of the progress bar
     if isinstance(progress, int):
         progress = float(progress)
     if not isinstance(progress, float):
@@ -22,64 +24,36 @@ def update_progress(progress):
     text = "Progress: [{0}] {1:.1f}%".format(
         "#" * block + "-" * (bar_length - block), progress * 100
     )
-    print(text)
+    print(text,end="\r")
 
-
-current_path = os.path.abspath(os.getcwd())
-
-COCO_ANNOTATIONS_PATH = current_path + "\\annotations\\instances_train2017.json"
-COCO_IMAGES_DIRECTORY = current_path + "\\train2017\\"
-EXTRACTED_SAVING_PATH = current_path + "\\extracted_dataset\\"
-SAVE_FOLDER = EXTRACTED_SAVING_PATH.split("\\")[
-    len(EXTRACTED_SAVING_PATH.split("\\")) - 2
-]
-
-if not os.path.exists(EXTRACTED_SAVING_PATH):
-    os.mkdir(EXTRACTED_SAVING_PATH)
-
-coco = COCO(COCO_ANNOTATIONS_PATH)
-
-cats = coco.loadCats(coco.getCatIds())
-
-nms = [cat["name"] for cat in cats]
-
-target_classes = []
-with open(current_path + "/classes.txt", "r") as f:
-    for line in f.readlines():
-        target_classes.append(line.strip())
-
-img_dict = {}
-for classes in target_classes:
-    catIds = coco.getCatIds(catNms=[classes])
-    imgIds = coco.getImgIds(catIds=catIds)
-    for imgID in imgIds:
-        try:
-            content = ""
-            content = img_dict[imgID] + ","
-        except:
-            pass
-        img_dict[imgID] = content + classes
-
+# Function to output the data in TensorFlow CSV format
 def tensorflowFormat(img_dict):
     total_progress = len(img_dict)
     progress = 0
-    with open("train.csv",mode='w+',encoding='utf8') as f:
-        # write csv head
+    with open("train.csv", mode="w+", encoding="utf8") as f:
+        # Write CSV header
         f.write("filename,width,height,class,xmin,ymin,xmax,ymax\n")
-        
+
         for img_id in img_dict:
             progress += 1
             annotation_ids = coco.getAnnIds(img_id)
             annotations = coco.loadAnns(annotation_ids)
 
             image_meta = coco.loadImgs(annotations[0]["image_id"])[0]
-
-            f.write(f"{image_meta['file_name']},{image_meta['height']},{image_meta['width']},")
-            for i in annotations:
-                
+            for ann in annotations:
+                entity_id = ann["category_id"]
+                entity = coco.loadCats(entity_id)[0]["name"]
+                if entity in target_classes:
+                    bbox = ann["bbox"]
+                    f.write(
+                        f"{image_meta['file_name']},{image_meta['width']},{image_meta['height']},"
+                        f"{entity},{int(bbox[0])},{int(bbox[1])},{int(bbox[0] + bbox[2])},{int(bbox[1] + bbox[3])}\n"
+                    )
 
             update_progress(progress / total_progress)
 
+
+# Function to output the data in COCO XML format
 def cocoFormat(img_dict):
     total_progress = len(img_dict)
     progress = 0
@@ -102,8 +76,8 @@ def cocoFormat(img_dict):
         node_depth = SubElement(node_size, "depth")
         node_depth.text = "3"
 
-        for i in range(len(annotations)):
-            entity_id = annotations[i]["category_id"]
+        for ann in annotations:
+            entity_id = ann["category_id"]
             entity = coco.loadCats(entity_id)[0]["name"]
             if entity in target_classes:
                 node_object = SubElement(node_root, "object")
@@ -112,29 +86,75 @@ def cocoFormat(img_dict):
                 node_difficult = SubElement(node_object, "difficult")
                 node_difficult.text = "0"
                 node_bndbox = SubElement(node_object, "bndbox")
+                bbox = ann["bbox"]
                 node_xmin = SubElement(node_bndbox, "xmin")
-                node_xmin.text = str(round(annotations[i]["bbox"][0]))
+                node_xmin.text = str(round(bbox[0]))
                 node_ymin = SubElement(node_bndbox, "ymin")
-                node_ymin.text = str(round(annotations[i]["bbox"][1]))
+                node_ymin.text = str(round(bbox[1]))
                 node_xmax = SubElement(node_bndbox, "xmax")
-                node_xmax.text = str(
-                    round(annotations[i]["bbox"][0] + annotations[i]["bbox"][2])
-                )
+                node_xmax.text = str(round(bbox[0] + bbox[2]))
                 node_ymax = SubElement(node_bndbox, "ymax")
-                node_ymax.text = str(
-                    round(annotations[i]["bbox"][1] + annotations[i]["bbox"][3])
-                )
+                node_ymax.text = str(round(bbox[1] + bbox[3]))
 
         xml = tostring(node_root, pretty_print=True)
         dom = parseString(xml)
         with open(
-            EXTRACTED_SAVING_PATH + image_meta["file_name"].split(".")[0] + ".xml", "w"
+            os.path.join(
+                EXTRACTED_SAVING_PATH, image_meta["file_name"].split(".")[0] + ".xml"
+            ),
+            "w",
         ) as xml_file:
             xml_file.write(dom.toxml())
 
         copyfile(
-            COCO_IMAGES_DIRECTORY + image_meta["file_name"],
-            EXTRACTED_SAVING_PATH + image_meta["file_name"],
+            os.path.join(COCO_IMAGES_DIRECTORY, image_meta["file_name"]),
+            os.path.join(EXTRACTED_SAVING_PATH, image_meta["file_name"]),
         )
 
         update_progress(progress / total_progress)
+
+
+# Get the current working directory
+current_path = os.path.abspath(os.getcwd())
+
+# Define paths for COCO annotations, images, and extracted dataset
+COCO_ANNOTATIONS_PATH = os.path.join(
+    current_path,LABEL_FOLDER
+)
+COCO_IMAGES_DIRECTORY = os.path.join(current_path,IMAGE_FOLDER)
+EXTRACTED_SAVING_PATH = os.path.join(current_path, "extracted_dataset")
+SAVE_FOLDER = os.path.basename(os.path.dirname(EXTRACTED_SAVING_PATH))
+
+# Create the directory for extracted dataset if it doesn't exist
+if not os.path.exists(EXTRACTED_SAVING_PATH):
+    os.mkdir(EXTRACTED_SAVING_PATH)
+
+# Load COCO annotations
+coco = COCO(COCO_ANNOTATIONS_PATH)
+
+# Load all categories
+cats = coco.loadCats(coco.getCatIds())
+nms = [cat["name"] for cat in cats]
+
+# Read target classes from the classes.txt file
+target_classes = []
+with open(os.path.join(current_path, "classes.txt"), "r") as f:
+    for line in f.readlines():
+        target_classes.append(line.strip())
+
+# Create a dictionary to store image IDs and their corresponding classes
+img_dict = {}
+for classes in target_classes:
+    catIds = coco.getCatIds(catNms=[classes])
+    imgIds = coco.getImgIds(catIds=catIds)
+    for imgID in imgIds:
+        content = img_dict.get(imgID, "")
+        if content:
+            content += ","
+        img_dict[imgID] = content + classes
+
+
+
+# Run the desired output format function
+tensorflowFormat(img_dict)
+# cocoFormat(img_dict)
